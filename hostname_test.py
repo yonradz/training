0b1ff8defb21c1aee1e15d0ddc9c7d4046d3ba71
Michael Ben-Ami <mike.benami@gmail.com>
2015-10-23 12:56:37 -0400

    changed hostname_test

--- hostname_test.py
+++ hostname_test.py
@@ -7,7 +7,7 @@ FAILURE = 1
 SUCCESS = 0
 
 for snippet_file in os.listdir(SNIPPET_DIR):
-    with open(snippet_file) as snippet_file_handler:
+    with open(os.path.join(SNIPPET_DIR, snippet_file)) as snippet_file_handler:
         file_contents = snippet_file_handler.read()
         if ('hostname ' + HOSTNAME) not in file_contents:
             sys.exit(FAILURE)
